00fe785a13cacefe08e59ca266e6ce02b590112e
Patrick <patik.developer@outlook.com>
2023-08-22 19:23:15 +0200

    New translations response.php (Slovenian)

--- lang/sl/response.php
+++ lang/sl/response.php
@@ -251,6 +251,12 @@ return [
             'super_admin_cannot_be_updated' => 'Super Admin vloge ni mogoče posodobiti',
             'super_admin_cannot_be_deleted' => 'Super Admin vloge ni mogoče izbrisati',
         ],
+        'company_logo' => [
+            'upload_success' => 'Company logo was uploaded successfully',
+            'upload_failed' => 'Company logo could not be uploaded',
+            'remove_success' => 'Company logo was removed successfully',
+            'remove_failed' => 'Company logo could not be removed',
+        ],
         'settings' => [
             'update_success' => 'Nastavitve so bile uspešno posodobljene',
             'update_failed' => 'Nastavitve ni bilo mogoče posodobiti',
