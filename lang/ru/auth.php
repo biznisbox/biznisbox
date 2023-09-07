040a55a5c36cd56a52646c9592f572e6e953a608
Patrick <patik.developer@outlook.com>
2023-09-07 18:13:18 +0200

    New translations auth.php (Russian)

---
+++ lang/ru/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Учетные данные не совпадают с нашими записями.',
+    'password' => 'Указанный пароль неверен.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте снова через :seconds секунд.',
+];
