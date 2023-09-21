54780247369eb750684c954f33cd7acc32efe3ab
Patrick <patik.developer@outlook.com>
2023-09-21 20:49:46 +0200

    New translations auth.php (Chinese Simplified)

---
+++ lang/zh/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '这些凭据不符合我们的记录。',
+    'password' => '提供的密码不正确。',
+    'throttle' => '登录尝试次数太多。请在 :seconds 秒后重试。',
+];
