97272dc684ec0925a99327c95d011f4180f7bf54
Patrick <patik.developer@outlook.com>
2023-09-21 20:49:29 +0200

    New translations auth.php (Swedish)

---
+++ lang/sv/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Dessa referenser matchar inte våra register.',
+    'password' => 'Det angivna lösenordet är felaktigt.',
+    'throttle' => 'För många inloggningsförsök. Försök igen om :seconds sekunder.',
+];
