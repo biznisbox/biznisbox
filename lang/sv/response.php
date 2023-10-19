afe2423e1c17daf45f29030c07a8eb75d53d61e1
Patrick <patik.developer@outlook.com>
2023-10-19 22:51:16 +0200

    New translations response.php (Swedish)

--- lang/sv/response.php
+++ lang/sv/response.php
@@ -283,4 +283,8 @@ return [
             'delete_failed_self_account' => 'Du kan inte ta bort ditt eget konto',
         ],
     ],
+    'memory_used' => 'Memory used',
+    'memory_available' => 'Memory available',
+    'disk_used' => 'Disk used',
+    'disk_available' => 'Disk available',
 ];
