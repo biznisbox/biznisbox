b9f34b648fa463c1b864c5deb332478046811ef4
Patrick <patik.developer@outlook.com>
2023-10-19 22:50:45 +0200

    New translations response.php (Greek)

--- lang/el/response.php
+++ lang/el/response.php
@@ -283,4 +283,8 @@ return [
             'delete_failed_self_account' => 'Δεν μπορείτε να διαγράψετε τον δικό σας λογαριασμό',
         ],
     ],
+    'memory_used' => 'Memory used',
+    'memory_available' => 'Memory available',
+    'disk_used' => 'Disk used',
+    'disk_available' => 'Disk available',
 ];
