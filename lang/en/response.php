87d9f9338a4c124dc45e9488e4ac7370a1542fff
Patrick <patik.developer@outlook.com>
2023-12-26 13:09:42 +0100

    New translations response.php (English)

--- lang/en/response.php
+++ lang/en/response.php
@@ -98,6 +98,19 @@ return [
         'delete_success' => 'Document deleted successfully',
     ],
 
+    // Employee responses
+    'employee' => [
+        'not_found' => 'Employee not found',
+        'get_success' => 'Employees retrieved successfully',
+        'get_failed' => 'Employees could not be retrieved',
+        'create_success' => 'Employee created successfully',
+        'create_failed' => 'A Employee could not be created',
+        'update_success' => 'Employee updated successfully',
+        'update_failed' => 'A Employee could not be updated',
+        'delete_success' => 'Employee deleted successfully',
+        'delete_failed' => 'A Employee could not be deleted',
+    ],
+
     // Archive responses
     'archive' => [
         'get_success' => 'File retrieved successfully',
