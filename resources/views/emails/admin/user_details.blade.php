7589a0266e5151f9f2a896ff05355a0efea0f985
Patrick <69119220+patik123@users.noreply.github.com>
2023-12-27 13:39:07 +0100

    💄 update style of emails and remove old emails templates

--- resources/views/emails/admin/user_details.blade.php
+++ resources/views/emails/admin/user_details.blade.php
@@ -1,15 +1,23 @@
 @extends('emails.layout')
-
 @section('content')
-    <p>Hi,</p>
-    <p>A user account was created today.</p>
-
-    <p>Here are the details:</p>
-
-    <p>Username: {{ $user['email'] }}</p>
-    <p>Password: {{ $user['password'] }}</p>
-
-    <a href="{{ $url }}">For login use this link. </a>
-    <p>Regards,</p>
-    <p>{{ $app }}</p>
+<p>Hi {{ $user->name }},</p>
+<p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
+<p>Please click on the button below to reset your password:</p>
+<br />
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+   <tbody>
+      <tr>
+         <td align="left">
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+               <tbody>
+                  <tr>
+                     <td> <a href="{{ $url }}" target="_blank">Reset Password</a> </td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+<p>If you did not request a password reset, no further action is required.</p>
 @endsection
--- resources/views/emails/auth/reset_password.blade.php
+++ resources/views/emails/auth/reset_password.blade.php
@@ -1,8 +1,23 @@
 @extends('emails.layout')
-
 @section('content')
-    <p>Hi {{ $user->name }},</p>
-    <p>You are receiving this email because we received a password reset request for your account.</p>
-    <p><a href="{{ $url }}">Reset Password</a></p>
-    <p>If you did not request a password reset, no further action is required.</p>
+<p>Hi {{ $user->first_name . ' ' . $user->last_name }},</p>
+<p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
+<p>Please click on the button below to reset your password:</p>
+<br />
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+   <tbody>
+      <tr>
+         <td align="left">
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+               <tbody>
+                  <tr>
+                     <td> <a href="{{ $url }}" target="_blank">Reset Password</a> </td>
+                  </tr>
+               </tbody>
+            </table>
+         </td>
+      </tr>
+   </tbody>
+</table>
+<p>If you did not request a password reset, no further action is required.</p>
 @endsection
