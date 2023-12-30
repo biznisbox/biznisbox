b86060d44d76cbcabd4a41647ad9a47e4cd8616c
Patrick <patik.developer@outlook.com>
2023-12-30 19:41:16 +0100

    New translations auth.php (Turkish)

--- lang/tr/auth.php
+++ lang/tr/auth.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Bu kimlik bilgileri kayıtlarımızla uyuşmuyor.',
+    'password' => 'Sağlanan şifre yanlış.',
+    'throttle' => 'Çok fazla giriş denemesi yapıldı. Lütfen :seconds saniye sonra tekrar deneyin.',
 ];
