77f371ae2281aa45e8105949bb0645b3580a3b42
Patrick <patik.developer@outlook.com>
2023-12-30 19:41:10 +0100

    New translations auth.php (Serbian (Cyrillic))

--- lang/sr/auth.php
+++ lang/sr/auth.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ове акредитације не одговарају нашим записима.',
+    'password' => 'Достављена лозинка је нетачна.',
+    'throttle' => 'Превише покушаја пријаве. Молим вас покушајте поново за :seconds секунди.',
 ];
