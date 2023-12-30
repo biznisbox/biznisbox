39549870090921f889758bc9595146df78e967dd
Patrick <patik.developer@outlook.com>
2023-12-30 19:41:38 +0100

    New translations auth.php (Franconian)

--- lang/fra/auth.php
+++ lang/fra/auth.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Disse Credentials passen net zu unseren Akten.',
+    'password' => 'Das bereitgestellte Passwort ist inkorrekt.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuche es in :seconds Sekunden erneut.',
 ];
