d6b8e9f45389c2acaad10d6e93d3958a50a87a3e
Patrick <patik.developer@outlook.com>
2023-12-30 19:40:21 +0100

    New translations auth.php (Catalan)

--- lang/ca/auth.php
+++ lang/ca/auth.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
+    'password' => 'La contrasenya proporcionada és incorrecta.',
+    'throttle' => 'Massa intents d\'inici de sessió. Si us plau, torna a provar en :seconds segons.',
 ];
