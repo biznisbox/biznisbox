1dbdc5df2f5d093f9b73be9b8cbf5b40d75d57c7
Patrick <patik.developer@outlook.com>
2024-01-20 22:32:40 +0100

    New translations response.php (Czech)

---
+++ lang/cs/response.php
@@ -0,0 +1,337 @@
+<?php
+
+return [
+    'success' => 'Success',
+    'failed' => 'Failed',
+    'not_found' => 'Not found',
+    'create_success' => 'Created successfully',
+    'create_failed' => 'Could not be created',
+    'update_success' => 'Updated successfully',
+    'update_failed' => 'Could not be updated',
+    'delete_success' => 'Deleted successfully',
+    'delete_failed' => 'Could not be deleted',
+    'get_success' => 'Retrieved successfully',
+    'get_failed' => 'Could not be retrieved',
+    'upload_success' => 'Uploaded successfully',
+    'upload_failed' => 'Could not be uploaded',
+    'remove_success' => 'Removed successfully',
+    'remove_failed' => 'Could not be removed',
+    'send_success' => 'Sent successfully',
+    'send_failed' => 'Could not be sent',
+    'share_success' => 'Shared successfully',
+    'share_failed' => 'Could not be shared',
+    'accept_reject_success' => 'Accepted/rejected successfully',
+    'convert_success' => 'Converted successfully',
+    // Auth responses
+    'login' => [
+        'success' => 'Úspěšně jste se přihlásili',
+        'failed' => 'Přihlášení se nezdařilo',
+    ],
+    'logout' => [
+        'success' => 'Odhlášení bylo úspěšné',
+        'failed' => 'Odhlášení se nezdařilo',
+    ],
+    'auth' => [
+        'failed' => 'Ověření se nezdařilo',
+    ],
+    'reset' => [
+        'success' => 'Obnovení hesla bylo úspěšné',
+        'failed' => 'Obnovení hesla se nezdařilo',
+    ],
+
+    // Product responses
+    'product' => [
+        'not_found' => 'Produkt nebyl nalezen',
+        'get_success' => 'Produkty úspěšně načteny',
+        'get_failed' => 'Produkty nelze načíst',
+        'create_success' => 'Produkt byl úspěšně vytvořen',
+        'create_failed' => 'Produkt nelze vytvořit',
+        'update_success' => 'Produkt byl úspěšně aktualizován',
+        'update_failed' => 'Produkt nemohl být aktualizován',
+        'delete_success' => 'Produkt byl úspěšně odstraněn',
+        'delete_failed' => 'Produkt nelze odstranit',
+    ],
+
+    // Partner responses
+    'partner' => [
+        'not_found' => 'Partner nebyl nalezen',
+        'get_success' => 'Partneři úspěšně načteni',
+        'get_failed' => 'Partneři nelze načíst',
+        'create_success' => 'Partner byl úspěšně vytvořen',
+        'create_failed' => 'Nelze vytvořit partnera',
+        'update_success' => 'Partner byl úspěšně aktualizován',
+        'update_failed' => 'Partner nemohl být aktualizován',
+        'delete_success' => 'Partner byl úspěšně odstraněn',
+        'delete_failed' => 'Partner nemohl být odstraněn',
+    ],
+
+    // Transaction responses
+    'transaction' => [
+        'not_found' => 'Transakce nenalezena',
+        'get_success' => 'Transakce úspěšně načteny',
+        'get_failed' => 'Transakce nelze načíst',
+        'create_success' => 'Transakce byla úspěšně vytvořena',
+        'create_failed' => 'Transakci nelze vytvořit',
+        'update_success' => 'Transakce byla úspěšně aktualizována',
+        'update_failed' => 'Transakci nelze aktualizovat',
+        'delete_success' => 'Transakce byla úspěšně odstraněna',
+        'delete_failed' => 'Transakci nelze odstranit',
+    ],
+
+    //Account responses
+    'accounts' => [
+        'get_success' => 'Účty úspěšně načteny',
+        'get_failed' => 'Účty nelze načíst',
+        'not_found' => 'Účet nebyl nalezen',
+        'create_success' => 'Účet byl úspěšně vytvořen',
+        'create_error' => 'Účet nelze vytvořit',
+        'update_success' => 'Účet byl úspěšně aktualizován',
+        'update_error' => 'Účet nelze aktualizovat',
+        'delete_success' => 'Účet byl úspěšně odstraněn',
+        'delete_error' => 'Účet nelze odstranit',
+        'get_success' => 'Účet byl úspěšně načten',
+        'get_error' => 'Účet nelze načíst',
+        'delete_default_account' => 'Výchozí účet nelze odstranit',
+    ],
+
+    //Bill responses
+    'bill' => [
+        'get_success' => 'Faktura byla úspěšně načtena',
+        'get_error' => 'Faktura nemohla být načtena',
+        'create_success' => 'Faktura byla úspěšně vytvořena',
+        'create_error' => 'Faktura nemohla být vytvořena',
+        'update_success' => 'Faktura byla úspěšně aktualizována',
+        'update_error' => 'Fakturaci nelze aktualizovat',
+        'delete_success' => 'Faktura byla úspěšně odstraněna',
+        'delete_error' => 'Faktura nemohla být úspěšně odstraněna',
+    ],
+
+    //Document responses
+    'document' => [
+        'get_success' => 'Dokumenty byly úspěšně načteny',
+        'get_error' => 'Dokumenty nelze načíst',
+        'not_found' => 'Doklad nebyl nalezen',
+        'create_failed' => 'Doklad nelze vytvořit',
+        'create_success' => 'Dokument byl úspěšně vytvořen',
+        'update_success' => 'Dokument byl úspěšně aktualizován',
+        'update_error' => 'Dokument nelze aktualizovat',
+        'delete_failed' => 'Doklad nemohl být úspěšně odstraněn',
+        'delete_success' => 'Doklad byl úspěšně odstraněn',
+    ],
+
+    // Employee responses
+    'employee' => [
+        'not_found' => 'Zaměstnanec nebyl nalezen',
+        'get_success' => 'Zaměstnanci úspěšně načteni',
+        'get_failed' => 'Zaměstnanci nelze získat',
+        'create_success' => 'Zaměstnanec úspěšně vytvořen',
+        'create_failed' => 'Zaměstnanec nemohl být vytvořen',
+        'update_success' => 'Zaměstnanec úspěšně aktualizován',
+        'update_failed' => 'Zaměstnanec nelze aktualizovat',
+        'delete_success' => 'Zaměstnanec byl úspěšně odstraněn',
+        'delete_failed' => 'Zaměstnanec nemohl být odstraněn',
+    ],
+
+    // Archive responses
+    'archive' => [
+        'get_success' => 'Soubor byl úspěšně načten',
+        'get_error' => 'Soubor nelze načíst',
+        'not_found' => 'Soubor nebyl nalezen',
+        'create_failed' => 'Soubor nelze vytvořit',
+        'create_success' => 'Soubor byl úspěšně vytvořen',
+        'update_success' => 'Soubor byl úspěšně aktualizován',
+        'update_failed' => 'Soubor nelze aktualizovat',
+        'delete_failed' => 'Soubor nemohl být úspěšně odstraněn',
+        'delete_success' => 'Soubor byl úspěšně odstraněn',
+        'get_success_folder' => 'Složka úspěšně načtena',
+        'get_error_folder' => 'Složky nelze načíst',
+        'not_found_folder' => 'Složka nebyla nalezena',
+        'create_failed_folder' => 'Složku nelze vytvořit',
+        'create_success_folder' => 'Složka byla úspěšně vytvořena',
+        'update_success_folder' => 'Složka byla úspěšně aktualizována',
+        'update_failed_folder' => 'Složku nelze aktualizovat',
+        'delete_failed_folder' => 'Složku nelze úspěšně odstranit',
+        'delete_success_folder' => 'Složka byla úspěšně smazána',
+    ],
+
+    //Quote responses
+    'quote' => [
+        'get_success' => 'Nabídky byly úspěšně načteny',
+        'get_failed' => 'Cenové nabídky nelze načíst',
+        'not_found' => 'Cenová nabídka nebyla nalezena',
+        'create_failed' => 'Cenovou nabídku nelze vytvořit',
+        'create_success' => 'Cenová nabídka byla úspěšně vytvořena',
+        'convert_success' => 'Cenová nabídka byla úspěšně převedena na fakturu',
+        'update_success' => 'Cenová nabídka byla úspěšně aktualizována',
+        'delete_success' => 'Cenová nabídka byla úspěšně odstraněna',
+        'delete_failed' => 'Cenovou nabídku nelze odstranit',
+        'update_failed' => 'Cenovou nabídku nelze aktualizovat',
+        'share_success' => 'Cenová nabídka byla úspěšně sdílena',
+        'share_failed' => 'Cenovou nabídku nelze sdílet',
+        'accept_reject_success' => 'Cenová nabídka byla úspěšně odmítnuta',
+        'send_success' => 'Cenová nabídka byla úspěšně odeslána',
+        'send_failed' => 'Cenovou nabídku nelze odeslat',
+        'pdf_failed' => 'Generování PDF selhalo',
+    ],
+
+    //Invoice responses
+    'invoice' => [
+        'get_success' => 'Faktury úspěšně načteny',
+        'get_failed' => 'Faktury nelze načíst',
+        'not_found' => 'Faktura nebyla nalezena',
+        'create_failed' => 'Faktura nemohla být vytvořena',
+        'create_success' => 'Faktura byla úspěšně vytvořena',
+        'delete_success' => 'Faktura byla úspěšně odstraněna',
+        'update_success' => 'Faktura byla úspěšně aktualizována',
+        'share_success' => 'Faktura byla úspěšně sdílena',
+        'share_failed' => 'Cenovou nabídku nelze sdílet',
+        'send_success' => 'Faktura byla úspěšně odeslána',
+        'pdf_failed' => 'Generování PDF selhalo',
+        'send_failed' => 'Faktura nemohla být odeslána',
+        'update_failed' => 'Fakturu nelze aktualizovat',
+        'delete_failed' => 'Faktura nemohla být odstraněna',
+        'transaction_success' => 'Transakce byla úspěšně přidána',
+    ],
+
+    //Online payment responses
+    'payment' => [
+        'stripe_not_available' => 'Stripe není k dispozici',
+        'already_paid' => 'Faktura již byla zaplacena',
+        'invalid_key' => 'Neplatný klíč API',
+        'not_found' => 'Platba nebyla nalezena',
+        'success' => 'Platba byla úspěšná',
+        'failed' => 'Platba se nezdařila',
+        'paypal_not_available' => 'PayPal není k dispozici',
+        'invoice' => 'Faktura',
+    ],
+
+    //Profile responses
+    'user' => [
+        'not_found' => 'Uživatel nebyl nalezen',
+        'update_success' => 'Uživatel byl úspěšně aktualizován',
+        'password_updated' => 'Heslo bylo úspěšně aktualizováno',
+        'password_not_match' => 'Heslo se neshoduje',
+        'password_empty' => 'Pole pro heslo je prázdné',
+        'avatar_deleted' => 'Avatar was deleted successfully',
+        'avatar_not_found' => 'Avatar could not be found',
+        'avatar_updated' => 'Avatar was updated successfully',
+    ],
+
+    // Open Banking responses
+    'open_banking' => [
+        'country_required' => 'Země je povinná',
+        'not_enabled' => 'Otevřené bankovnictví není povoleno',
+        'not_found' => 'Otevřené bankovnictví nenalezeno',
+        'id_required' => 'ID je povinné',
+        'not_provided_infos' => 'Neposkytnuté informace',
+        'requisition_success' => 'Požadavek byl úspěšný',
+        'requisition_failed' => 'Požadavek se nezdařil',
+        'session_id_required' => 'Je vyžadováno ID relace',
+        'account_id_required' => 'Je vyžadováno ID účtu',
+    ],
+
+    'calendar' => [
+        'get_success' => 'Událost úspěšně načtena',
+        'get_error' => 'Událost nelze načíst',
+        'not_found' => 'Událost nebyla nalezena',
+        'create_failed' => 'Událost nemohla být vytvořena',
+        'create_success' => 'Událost byla úspěšně vytvořena',
+        'update_success' => 'Událost byla úspěšně aktualizována',
+        'update_failed' => 'Událost nemohla být aktualizována',
+        'delete_failed' => 'Událost nemohla být úspěšně odstraněna',
+        'delete_success' => 'Událost byla úspěšně odstraněna',
+    ],
+    'department' => [
+        'get_success' => 'Oddělení úspěšně načteno',
+        'get_error' => 'Oddělení nelze získat',
+        'not_found' => 'Oddělení nebylo nalezeno',
+        'create_failed' => 'Oddělení nelze vytvořit',
+        'create_success' => 'Oddělení bylo úspěšně vytvořeno',
+        'update_success' => 'Oddělení bylo úspěšně aktualizováno',
+        'update_failed' => 'Oddělení nelze aktualizovat',
+        'delete_failed' => 'Oddělení nelze úspěšně odstranit',
+        'delete_success' => 'Oddělení bylo úspěšně odstraněno',
+    ],
+    // Email responses
+    'email' => [
+        'invoice_subject' => 'Faktura',
+    ],
+
+    'dashboard' => [
+        'income' => 'Výnosy',
+        'expense' => 'Výdaje',
+    ],
+
+    'months' => [
+        'january' => 'Leden',
+        'february' => 'únor',
+        'march' => 'Březen',
+        'april' => 'duben',
+        'may' => 'Maj',
+        'june' => 'Červen',
+        'july' => 'červenec',
+        'august' => 'Srpen',
+        'september' => 'Září',
+        'october' => 'Říjen',
+        'november' => 'Listopad',
+        'december' => 'prosinec',
+    ],
+
+    'admin' => [
+        'role' => [
+            'not_found' => 'Role nebyla nalezena',
+            'create_failed' => 'Role nemohla být vytvořena',
+            'create_success' => 'Role byla úspěšně vytvořena',
+            'update_success' => 'Role byla úspěšně aktualizována',
+            'delete_success' => 'Role byla úspěšně smazána',
+            'delete_failed' => 'Role nemohla být odstraněna',
+            'super_admin_cannot_be_created' => 'Super administrátorská role nemůže být vytvořena',
+            'super_admin_cannot_be_updated' => 'Super administrátorská role nemůže být aktualizována',
+            'super_admin_cannot_be_deleted' => 'Super administrátorská role nemůže být odstraněna',
+        ],
+        'company_logo' => [
+            'upload_success' => 'Logo společnosti bylo úspěšně nahráno',
+            'upload_failed' => 'Logo společnosti nelze nahrát',
+            'remove_success' => 'Logo společnosti bylo úspěšně odstraněno',
+            'remove_failed' => 'Logo společnosti nelze odstranit',
+        ],
+        'settings' => [
+            'update_success' => 'Nastavení bylo úspěšně aktualizováno',
+            'update_failed' => 'Nastavení nelze aktualizovat',
+        ],
+        'currency' => [
+            'not_found' => 'Měnu nelze nalézt',
+            'create_failed' => 'Měnu nelze vytvořit',
+            'create_success' => 'Měna byla úspěšně vytvořena',
+            'update_success' => 'Měna byla úspěšně aktualizována',
+            'delete_success' => 'Měna byla úspěšně smazána',
+            'delete_failed' => 'Měnu nelze odstranit',
+            'rates_updated' => 'Sazby byly úspěšně aktualizovány',
+        ],
+        'taxes' => [
+            'not_found' => 'Daň nebyla nalezena',
+            'create_failed' => 'Daň nelze vytvořit',
+            'create_success' => 'Daň byla úspěšně vytvořena',
+            'update_success' => 'Daň byla úspěšně aktualizována',
+            'delete_success' => 'Daň byla úspěšně odstraněna',
+            'delete_failed' => 'Daň nelze odstranit',
+        ],
+
+        'user' => [
+            'not_found' => 'Uživatel nebyl nalezen',
+            'create_failed' => 'Uživatele nelze vytvořit',
+            'create_success' => 'Uživatel byl úspěšně vytvořen',
+            'update_success' => 'Uživatel byl úspěšně aktualizován',
+            'update_failed' => 'Uživatele nelze aktualizovat',
+            'delete_success' => 'Uživatel byl úspěšně odstraněn',
+            'delete_failed' => 'Uživatele nelze odstranit',
+            'password_reset_success' => 'Heslo bylo úspěšně obnoveno',
+            'password_reset_failed' => 'Heslo nelze obnovit',
+            'delete_failed_self_account' => 'Nemůžete smazat svůj vlastní účet',
+        ],
+    ],
+    'memory_used' => 'Použitá paměť',
+    'memory_available' => 'Dostupná paměť',
+    'disk_used' => 'Použitý disk',
+    'disk_available' => 'Disk k dispozici',
+];
