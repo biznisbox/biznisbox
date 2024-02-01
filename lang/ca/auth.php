d3c6ff34be67e91439a2ce4fe66f89c2fcdb6d97
Patrick <patik.developer@outlook.com>
2024-02-01 11:09:58 +0100

    New translations auth.php (Catalan)

---
+++ lang/ca/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
+    'password' => 'La contrasenya proporcionada és incorrecta.',
+    'throttle' => 'Massa intents d\'inici de sessió. Si us plau, torna a provar en :seconds segons.',
+];
