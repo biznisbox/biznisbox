db91b9c64cb7331e1d34b627a5b4b8be789b56e6
Patrick <patik.developer@outlook.com>
2024-02-01 11:11:28 +0100

    New translations auth.php (Serbian (Cyrillic))

---
+++ lang/sr/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ове акредитације не одговарају нашим записима.',
+    'password' => 'Достављена лозинка је нетачна.',
+    'throttle' => 'Превише покушаја пријаве. Молим вас покушајте поново за :seconds секунди.',
+];
