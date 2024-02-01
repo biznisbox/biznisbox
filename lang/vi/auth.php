2a1a91c270d02aaa5b36d7076875a35768912081
Patrick <patik.developer@outlook.com>
2024-02-01 11:12:05 +0100

    New translations auth.php (Vietnamese)

---
+++ lang/vi/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Thông tin đăng nhập không khớp với hồ sơ của chúng tôi.',
+    'password' => 'Mật khẩu không chính xác.',
+    'throttle' => 'Quá nhiều lần đăng nhập. Vui lòng thử lại sau :seconds giây.',
+];
