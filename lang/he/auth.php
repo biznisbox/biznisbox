7b6f9d55a9c2c628ab2b45b332661b16658cfe8a
Patrick <patik.developer@outlook.com>
2024-02-01 11:10:31 +0100

    New translations auth.php (Hebrew)

---
+++ lang/he/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'הפרטים האלה אינם תואמים לרשומות שלנו.',
+    'password' => 'הסיסמה שסופקה אינה נכונה.',
+    'throttle' => 'יותר מדי ניסיונות להתחברות. אנא נסה שוב בעוד :seconds שניות.',
+];
