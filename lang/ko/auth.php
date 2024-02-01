14a9ed29fc09630835793b0e3f9ceae4a525fae5
Patrick <patik.developer@outlook.com>
2024-02-01 11:10:52 +0100

    New translations auth.php (Korean)

---
+++ lang/ko/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '이 자격 증명은 우리의 기록과 일치하지 않습니다.',
+    'password' => '제공된 비밀번호가 잘못되었습니다.',
+    'throttle' => ':seconds 초 후에 다시 시도하십시오. 로그인 시도가 너무 많습니다.',
+];
