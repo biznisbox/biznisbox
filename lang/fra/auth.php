e8cab6a7facd301eca25d5e5c6c99a3fb4ee1ad8
Patrick <patik.developer@outlook.com>
2024-02-01 11:12:14 +0100

    New translations auth.php (Franconian)

---
+++ lang/fra/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Disse Credentials passen net zu unseren Akten.',
+    'password' => 'Das bereitgestellte Passwort ist inkorrekt.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuche es in :seconds Sekunden erneut.',
+];
