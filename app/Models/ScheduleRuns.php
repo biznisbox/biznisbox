4b07694ab8ddd151e6a0fc6f619c3faebaf305fe
Patrick <69119220+patik123@users.noreply.github.com>
2024-02-11 10:33:34 +0100

    🐛 fix schedule tasks saving in database

--- app/Models/ScheduleRuns.php
+++ app/Models/ScheduleRuns.php
@@ -18,12 +18,20 @@ class ScheduleRuns extends Model
         'created_at' => 'datetime',
     ];
 
+    /**
+     * Create a new task
+     *
+     * @param string $task - The task name
+     * @param string $status - The status of the task
+     * @param string $output - The output of the task (if any)
+     * @return void
+     */
     public static function createTask($task, $status, $output)
     {
-        $task = new ScheduleRuns();
-        $task->task = $task;
-        $task->status = $status;
-        $task->output = $output;
-        $task->save();
+        self::create([
+            'task' => $task,
+            'status' => $status,
+            'output' => $output,
+        ]);
     }
 }
