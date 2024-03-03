abe0926ebf0f2c78974975cd9a86fca72f7e6441
Patrick <patik.developer@outlook.com>
2024-03-03 12:05:57 +0100

    New translations response.php (Afrikaans)

--- lang/af/response.php
+++ lang/af/response.php
@@ -22,6 +22,11 @@ return [
     'share_failed' => 'Kon nie gedeel word nie',
     'accept_reject_success' => 'Suksesvol aanvaar/verwerp',
     'convert_success' => 'Suksesvol omgeskakel',
+    'pdf_failed' => 'Generating PDF failed',
+    'notification_success' => 'Notification was sent successfully',
+    'notification_failed' => 'Notification could not be sent',
+    'no_key_provided' => 'No key provided',
+    'ping' => 'API is working',
     // Auth responses
     'login' => [
         'success' => 'U het suksesvol aangemeld',
@@ -174,6 +179,18 @@ return [
         'pdf_failed' => 'Generering van PDF het misluk',
     ],
 
+    'support_ticket_message' => [
+        'get_success' => 'Support ticket messages retrieved successfully',
+        'get_failed' => 'Support ticket messages could not be retrieved',
+        'not_found' => 'Support ticket message could not be found',
+        'create_failed' => 'Support ticket message could not be created',
+        'create_success' => 'Support ticket message was created successfully',
+        'update_success' => 'Support ticket message was updated successfully',
+        'update_failed' => 'Support ticket message could not be updated',
+        'delete_failed' => 'Support ticket message could  not be deleted successfully',
+        'delete_success' => 'Support ticket message deleted successfully',
+    ],
+
     //Invoice responses
     'invoice' => [
         'get_success' => 'Invoices retrieved successfully',
@@ -205,6 +222,24 @@ return [
         'failed' => 'Betaling het misluk',
         'paypal_not_available' => 'Paypal nie beskikbaar nie',
         'invoice' => 'Faktuur',
+    ],
+
+    'support_ticket' => [
+        'get_success' => 'Support tickets retrieved successfully',
+        'get_failed' => 'Support tickets could not be retrieved',
+        'not_found' => 'Support ticket could not be found',
+        'create_failed' => 'Support ticket could not be created',
+        'create_success' => 'Support ticket was created successfully',
+        'update_success' => 'Support ticket was updated successfully',
+        'update_failed' => 'Support ticket could not be updated',
+        'delete_failed' => 'Support ticket could  not be deleted successfully',
+        'delete_success' => 'Support ticket deleted successfully',
+        'get_number_success' => 'Support ticket number retrieved successfully',
+        'get_number_failed' => 'Support ticket number could not be retrieved',
+        'send_success' => 'Support ticket message was sent successfully',
+        'send_failed' => 'Support ticket message could not be sent',
+        'share_success' => 'Support ticket was shared successfully',
+        'share_failed' => 'Support ticket could not be shared',
     ],
 
     //Profile responses
