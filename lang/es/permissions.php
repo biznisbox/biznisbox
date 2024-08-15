8304b640a5775a77feb630166dfa36bbe0145028
Patrick <patik.developer@outlook.com>
2024-08-15 21:32:34 +0200

    New translations permissions.php (Spanish)

--- lang/es/permissions.php
+++ lang/es/permissions.php
@@ -40,4 +40,5 @@ return [
     'super_admin' => 'Super Admin',
     'admin_email_settings' => 'Configuración de Email Admin',
     'admin_webhooks' => 'Admin Webhooks',
+    'webhooks' => 'Webhooks',
 ];
