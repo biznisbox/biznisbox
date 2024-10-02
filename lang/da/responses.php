46cdf55c3e04e30bed09e6442791677454a4223f
Patrick <patik.developer@outlook.com>
2024-10-02 22:30:36 +0200

    New translations responses.php (Danish)

--- lang/da/responses.php
+++ lang/da/responses.php
@@ -101,4 +101,8 @@ return [
     'install_admin_user_created' => 'Admin bruger oprettet med succes.',
     'item_not_moved' => 'Elementet er ikke flyttet',
     'item_moved_successfully' => 'Elementet blev flyttet',
+    'error_signing_contract' => 'Error signing contract',
+    'signer_not_found_or_already_signed' => 'Signer not found or already signed',
+    'cannot_delete_signed_contract' => 'Cannot delete signed contract',
+    'cannot_update_signed_contract' => 'Cannot update signed contract',
 ];
