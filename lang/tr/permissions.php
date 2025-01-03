4ff1e3cdb2dcad7420e6cc96f68468b44dcd0ae3
Patrick <patik.developer@outlook.com>
2025-01-03 21:32:14 +0100

    New translations permissions.php (Turkish)

--- lang/tr/permissions.php
+++ lang/tr/permissions.php
@@ -41,5 +41,5 @@ return [
     'admin_email_settings' => 'Yönetici E-posta Ayarları',
     'admin_webhooks' => 'Yönetici Webhooks',
     'webhooks' => 'Webhooks',
-    'contracts' => 'contracts',
+    'contracts' => 'Contracts',
 ];
