a95433c9513c931669f3a54e3c95fd5476b35c6e
Patrick <patik.developer@outlook.com>
2025-02-25 14:51:12 +0100

    New translations pdf.php (Slovenian)

--- lang/sl/pdf.php
+++ lang/sl/pdf.php
@@ -51,7 +51,7 @@ return [
     'payment_methods' => [
         'cash' => 'Gotovina',
         'check' => 'Ček',
-        'credit_card' => 'Bančna kartica',
+        'credit_card' => 'Payment card',
         'bank_transfer' => 'Bančno nakazilo',
         'paypal' => 'PayPal',
         'stripe' => 'Stripe',
