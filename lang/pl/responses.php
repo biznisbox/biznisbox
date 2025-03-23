4a98ee6fd33b57c4511073feafab310bc3d004f6
Patrick <patik.developer@outlook.com>
2025-03-23 15:32:34 +0100

    New translations responses.php (Polish)

--- lang/pl/responses.php
+++ lang/pl/responses.php
@@ -108,4 +108,6 @@ return [
     'cannot_update_signed_contract' => 'Nie można zaktualizować podpisanej umowy',
     'enable_js_to_use_app' => 'Przykro nam, ale ta aplikacja nie działa poprawnie bez włączonej obsługi JavaScript. Proszę włączyć ją, aby kontynuować.',
     'cannot_delete_own_account' => 'Nie można usunąć własnego konta',
+    'item_not_sent' => 'Produkt nie został wysłany',
+    'item_sent_successfully' => 'Element wysłany pomyślnie',
 ];
