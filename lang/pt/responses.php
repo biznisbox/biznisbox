ea124a69631b8456872a93f1a979589fb3b634d2
Patrick <patik.developer@outlook.com>
2025-03-23 15:32:37 +0100

    New translations responses.php (Portuguese)

--- lang/pt/responses.php
+++ lang/pt/responses.php
@@ -108,4 +108,6 @@ return [
     'cannot_update_signed_contract' => 'Não é possível atualizar o contrato assinado',
     'enable_js_to_use_app' => 'Lamentamos, mas este aplicativo não funciona corretamente sem JavaScript habilitado. Por favor, ative-o para continuar.',
     'cannot_delete_own_account' => 'Não é possível excluir a própria conta',
+    'item_not_sent' => 'Item não enviado',
+    'item_sent_successfully' => 'Item enviado com sucesso',
 ];
