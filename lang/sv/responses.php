df2fd2f8def91f974cf084c14c50a27241bf3dcc
Patrick <patik.developer@outlook.com>
2025-04-19 20:59:52 +0200

    New translations responses.php (Swedish)

--- lang/sv/responses.php
+++ lang/sv/responses.php
@@ -106,8 +106,7 @@ return [
     'signer_not_found_or_already_signed' => 'Signer hittades inte eller är redan signerad',
     'cannot_delete_signed_contract' => 'Kan inte ta bort undertecknat kontrakt',
     'cannot_update_signed_contract' => 'Kan inte uppdatera undertecknat kontrakt',
-    'enable_js_to_use_app' =>
-        'Vi beklagar, men den här appen fungerar inte ordentligt utan JavaScript aktiverat. Aktivera den för att fortsätta.',
+    'enable_js_to_use_app' => 'Vi beklagar, men den här appen fungerar inte ordentligt utan JavaScript aktiverat. Aktivera den för att fortsätta.',
     'cannot_delete_own_account' => 'Kan inte ta bort eget konto',
     'item_not_sent' => 'Objektet har inte skickats',
     'item_sent_successfully' => 'Objektet har skickats',
