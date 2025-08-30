163ebd9d8c53a4efea85345e98baa88471fbc692
Patrick <patik.developer@outlook.com>
2025-08-30 23:10:05 +0200

    New translations responses.php (Danish)

--- lang/da/responses.php
+++ lang/da/responses.php
@@ -127,4 +127,5 @@ return [
     ],
     'vat_number_not_valid' => 'CVR-nummer er ugyldigt',
     'vat_number_validated_successfully' => 'CVR-nummer valideret med succes',
+    'archive_document_auto_generated' => 'Automatisk generering af arkivdokument lykkedes'
 ];
