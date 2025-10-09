39d2fc5cb7c3ebb6c751993b74586a6568e3c178
Patrick <patik.developer@outlook.com>
2025-10-09 15:04:14 +0200

    New translations responses.php (Czech)

--- lang/cs/responses.php
+++ lang/cs/responses.php
@@ -106,8 +106,7 @@ return [
     'signer_not_found_or_already_signed' => 'Podpisník nebyl nalezen nebo již byl podepsán',
     'cannot_delete_signed_contract' => 'Nelze odstranit podepsanou smlouvu',
     'cannot_update_signed_contract' => 'Nelze aktualizovat podepsanou smlouvu',
-    'enable_js_to_use_app' =>
-        'Je nám líto, ale tato aplikace nefunguje správně, aniž by byl povolen JavaScript. Pro pokračování prosím povolte.',
+    'enable_js_to_use_app' => 'Je nám líto, ale tato aplikace nefunguje správně, aniž by byl povolen JavaScript. Pro pokračování prosím povolte.',
     'cannot_delete_own_account' => 'Nelze odstranit vlastní účet',
     'item_not_sent' => 'Položka nebyla odeslána',
     'item_sent_successfully' => 'Položka byla úspěšně odeslána',
