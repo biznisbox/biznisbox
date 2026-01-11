f9ad62591969e2a150f7d2b8f15cec13ebfc4208
Patrick <patik.developer@outlook.com>
2026-01-11 16:44:08 +0100

    New translations integrations.php (Russian)

---
+++ lang/ru/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Открытый банкинг',
+    'vies_vat' => 'Проверка НДС',
+    'document_signer' => 'Документ PDF',
+];
