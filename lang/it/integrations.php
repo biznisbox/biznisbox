7878237199a2188336f401305ff87860e007d116
Patrick <patik.developer@outlook.com>
2026-01-12 00:33:23 +0100

    New translations integrations.php (Italian)

--- lang/it/integrations.php
+++ lang/it/integrations.php
@@ -7,4 +7,5 @@ return [
     'open_banking' => 'Open Banking',
     'vies_vat' => 'Validazione Iva VIES',
     'document_signer' => 'Firmatario Documenti PDF',
+    'support_ticket_mailer' => 'Supporta L\'Integrazione Imap Ticket',
 ];
