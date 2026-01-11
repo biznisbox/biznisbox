8e00f9c9aaae5d2b74ca23650bbeca299dbedf2c
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:06 +0100

    New translations integrations.php (Danish)

---
+++ lang/da/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Møntbase',
+    'open_banking' => 'Åben Banking',
+    'vies_vat' => 'VIES Moms Validering',
+    'document_signer' => 'PDF Dokumentsignatur',
+];
