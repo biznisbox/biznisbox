32f06da6a6345b5ee1c8ec034b03deca20c4a552
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:30 +0100

    New translations integrations.php (Hungarian)

---
+++ lang/hu/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Open Banking',
+    'vies_vat' => 'VIES VAT Validation',
+    'document_signer' => 'PDF Document Signer',
+];
