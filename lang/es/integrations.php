447126403b15d7369d4507b7bbca4108789ebab7
Patrick <patik.developer@outlook.com>
2026-01-11 16:42:53 +0100

    New translations integrations.php (Spanish)

---
+++ lang/es/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Base de monedas',
+    'open_banking' => 'Abrir Banca',
+    'vies_vat' => 'VIES validación de IVA',
+    'document_signer' => 'Firma de documento PDF',
+];
