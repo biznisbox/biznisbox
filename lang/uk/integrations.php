7353bcdba29f7312bde5a485aa0a6c08fdf938ad
Patrick <patik.developer@outlook.com>
2026-01-11 16:44:29 +0100

    New translations integrations.php (Ukrainian)

---
+++ lang/uk/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Куенбаз',
+    'open_banking' => 'Відкрити банкінг',
+    'vies_vat' => 'Перевірка ПДВ VIES',
+    'document_signer' => 'Відправник PDF-документів',
+];
