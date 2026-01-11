9e2002c049cd9df977cfcf2f3583a6b72b667d75
Patrick <patik.developer@outlook.com>
2026-01-11 16:42:59 +0100

    New translations integrations.php (Czech)

---
+++ lang/cs/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Barevné hlinky',
+    'open_banking' => 'Otevřené bankovnictví',
+    'vies_vat' => 'VIES ověření DPH',
+    'document_signer' => 'PDF podpis dokumentu',
+];
