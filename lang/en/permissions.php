be28be42780f865bc7ff13c8765ccd79de3b202c
Patrick <patik.developer@outlook.com>
2026-01-11 16:44:44 +0100

    New translations permissions.php (English)

--- lang/en/permissions.php
+++ lang/en/permissions.php
@@ -41,4 +41,9 @@ return [
     'admin_email_settings' => 'Admin Email Settings',
     'admin_webhooks' => 'Admin Webhooks',
     'webhooks' => 'Webhooks',
+    'contracts' => 'Contracts',
+    'admin_status_page' => 'Admin Status Page',
+    'admin_update_app' => 'Admin Update App',
+    'admin_data_collection' => 'Admin Data Collection',
+    'projects' => 'Projects',
 ];
