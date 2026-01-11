3467b48c83bfbebbca9effc51fa411053a3338b0
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:42 +0100

    New translations integrations.php (Dutch)

---
+++ lang/nl/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'Paypal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Muntbasis',
+    'open_banking' => 'Open Bank',
+    'vies_vat' => 'VIES BTW validatie',
+    'document_signer' => 'PDF Document Handtekening',
+];
