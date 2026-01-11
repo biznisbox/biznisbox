cfe1182c04a2b6b6c8366bf70f0a1a4a662ae4cf
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:55 +0100

    New translations integrations.php (Polish)

---
+++ lang/pl/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Baza monet',
+    'open_banking' => 'Otwórz bankowość',
+    'vies_vat' => 'Weryfikacja VAT VIES',
+    'document_signer' => 'Podpisujący dokument PDF',
+];
