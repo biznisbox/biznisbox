e6dd97b5cdc187c6493029815f402675f9f9819d
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:27 +0100

    New translations permissions.php (Hungarian)

--- lang/hu/permissions.php
+++ lang/hu/permissions.php
@@ -41,4 +41,9 @@ return [
     'admin_email_settings' => 'Admin Email Beállítások',
     'admin_webhooks' => 'Admin Webhookok',
     'webhooks' => 'Webhorgonyok',
+    'contracts' => 'Contracts',
+    'admin_status_page' => 'Admin Status Page',
+    'admin_update_app' => 'Admin Update App',
+    'admin_data_collection' => 'Admin Data Collection',
+    'projects' => 'Projects',
 ];
