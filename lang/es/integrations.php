93015337c754de8019363b4b12ae70976a0e74d2
Patrick <patik.developer@outlook.com>
2026-01-12 00:33:16 +0100

    New translations integrations.php (Spanish)

--- lang/es/integrations.php
+++ lang/es/integrations.php
@@ -7,4 +7,5 @@ return [
     'open_banking' => 'Abrir Banca',
     'vies_vat' => 'VIES validación de IVA',
     'document_signer' => 'Firma de documento PDF',
+    'support_ticket_mailer' => 'Integración IMAP del ticket de soporte',
 ];
