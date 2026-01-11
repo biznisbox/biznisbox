9e0af3606a089513319e67b0db94dae4f89a0127
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:24 +0100

    New translations integrations.php (Finnish)

---
+++ lang/fi/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'Paypal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Avoin Pankkitoiminta',
+    'vies_vat' => 'VIES Alv-Vahvistus',
+    'document_signer' => 'Pdf Asiakirjan Allekirjoittaja',
+];
