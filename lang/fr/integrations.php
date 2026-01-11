dea8af59900bf57e8e56d5920df651a4b42918b8
Patrick <patik.developer@outlook.com>
2026-01-11 16:42:46 +0100

    New translations integrations.php (French)

---
+++ lang/fr/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Base de pièces',
+    'open_banking' => 'Ouvrir les services bancaires',
+    'vies_vat' => 'Validation de la TVA VIES',
+    'document_signer' => 'Signataire de document PDF',
+];
