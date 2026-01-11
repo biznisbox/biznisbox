7c7a999919041a74d550c2c313dd29902bcfaefa
Patrick <patik.developer@outlook.com>
2026-01-12 00:33:28 +0100

    New translations integrations.php (Russian)

--- lang/ru/integrations.php
+++ lang/ru/integrations.php
@@ -7,4 +7,5 @@ return [
     'open_banking' => 'Открытый банкинг',
     'vies_vat' => 'Проверка НДС',
     'document_signer' => 'Документ PDF',
+    'support_ticket_mailer' => 'Поддержка IMAP Интеграция с тикетом',
 ];
