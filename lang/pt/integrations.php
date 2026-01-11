761dfe24a7241b403f70007a05209f5747b87474
Patrick <patik.developer@outlook.com>
2026-01-11 16:44:01 +0100

    New translations integrations.php (Portuguese)

---
+++ lang/pt/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'Paypal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Abrir Banco',
+    'vies_vat' => 'VII - Validação de VAT',
+    'document_signer' => 'PDF Assinante de Documento',
+];
