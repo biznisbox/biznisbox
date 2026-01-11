96afbede29907244015622e11e9968000b063da5
Patrick <patik.developer@outlook.com>
2026-01-11 16:42:40 +0100

    New translations integrations.php (Slovenian)

---
+++ lang/sl/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Odprto bančništvo',
+    'vies_vat' => 'VIES VAT Validacija',
+    'document_signer' => 'PDF podpisovanje dokumentov',
+];
