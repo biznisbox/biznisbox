6fc5a3e8e6f591da25357cb01bd1dc65301b91b7
Patrick <patik.developer@outlook.com>
2026-01-12 00:33:32 +0100

    New translations integrations.php (Chinese Simplified)

--- lang/zh/integrations.php
+++ lang/zh/integrations.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'paypal' => 'PayPal',
+    'paypal' => 'Paypal',
     'stripe' => 'Stripe',
-    'coinbase' => 'Coinbase',
-    'open_banking' => 'Open Banking',
-    'vies_vat' => 'VIES VAT Validation',
-    'document_signer' => 'PDF Document Signer',
+    'coinbase' => '硬币',
+    'open_banking' => '开放银行',
+    'vies_vat' => 'VIES 增值税验证',
+    'document_signer' => 'PDF 文档签名者',
+    'support_ticket_mailer' => '支持服务单 IMAP 集成',
 ];
