762237d17eea416221d3b379422995d460637695
Patrick <patik.developer@outlook.com>
2026-01-11 16:43:18 +0100

    New translations integrations.php (Greek)

---
+++ lang/el/integrations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'paypal' => 'PayPal',
+    'stripe' => 'Stripe',
+    'coinbase' => 'Coinbase',
+    'open_banking' => 'Ανοικτή Τραπεζική',
+    'vies_vat' => 'VIES Επικύρωση ΦΠΑ',
+    'document_signer' => 'Υπογραφέας Εγγράφου Pdf',
+];
